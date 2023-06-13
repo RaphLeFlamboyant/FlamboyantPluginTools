266a6ad697a264ab95df96380135c43d67d9d021
CANDELARESI Raphaël <32449035+RaphLeFlamboyant@users.noreply.github.com>
2023-06-13 20:13:35 +0200

    Category setter on parameters

--- src/main/java/me/flamboyant/configurable/parameters/AParameter.java
+++ src/main/java/me/flamboyant/configurable/parameters/AParameter.java
@@ -8,20 +8,20 @@ public class AParameter {
     private String description;
     private String category;
 
-    public AParameter(Material representation, String parameterName, String description, String category) {
+    public AParameter(Material representation, String parameterName, String description)
+    {
         this.description = description;
         this.parameterName = parameterName;
         this.representation = representation;
-        this.category = category;
-    }
-
-    public AParameter(Material representation, String parameterName, String description)
-    {
-        this(representation, parameterName, description, "Misc");
+        this.category = "";
     }
 
     public Material getRepresentation() { return representation; }
     public String getParameterName() { return parameterName; }
     public String getDescription() { return description; }
     public String getCategory() { return category; }
+
+    public void setCategory(String categoryName) {
+        category = categoryName;
+    }
 }
