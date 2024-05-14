c62d06738077deff0f7abdcca759ab5a0d1e0b0d
CANDELARESI Raphaël <32449035+RaphLeFlamboyant@users.noreply.github.com>
2024-05-14 14:37:19 +0200

    Random real random

--- src/main/java/me/flamboyant/utils/Common.java
+++ src/main/java/me/flamboyant/utils/Common.java
@@ -3,10 +3,11 @@ package me.flamboyant.utils;
 import org.bukkit.Server;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Date;
 import java.util.Random;
 
 public class Common {
     public static JavaPlugin plugin;
     public static Server server;
-    public static Random rng = new Random();
+    public static Random rng = new Random((new Date()).getTime());
 }
